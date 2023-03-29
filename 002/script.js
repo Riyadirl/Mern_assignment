ce80dc08947980138151c32fe9afb1e418733eeb
Mahbub Hasaan Riyad <riyad.info1@gmail.com>
2023-03-29 21:26:44 +0600

    .

--- 002/script.js
+++ 002/script.js
@@ -1,10 +1,10 @@
 function calculateBMI() {
   var weight = document.getElementById("weightInput").value;
-  var height = document.getElementById("heightInput").value;
+  var height = document.getElementById("heightInput").value / 100;
 
   var bmi = weight / (height * height);
 
-  document.getElementById("bmiResult").innerHTML = "BMI is: " + bmi.toFixed();
+  document.getElementById("bmiResult").innerHTML = "BMI is: " + bmi.toFixed(2);
 }
 
 var calculateBtn = document.getElementById("calculateBtn");
