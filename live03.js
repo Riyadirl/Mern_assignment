bb152be92e53af1cf690d4ece78939b0e8569b90
Mahbub Hasaan Riyad <riyad.info1@gmail.com>
2023-04-07 23:49:08 +0600

    ..

--- live03.js
+++ live03.js
@@ -1,12 +1,13 @@
-function sumOfEvenSquares(arr) {
+function evenSum(arr) {
   var sum = 0;
   for (let i = 0; i < arr.length; i++) {
     if (arr[i] % 2 === 0) {
       sum = sum + Math.pow(arr[i], 2);
     }
   }
+
   return sum;
 }
 
 const arr = [1, 2, 3, 4, 5];
-console.log(sumOfEvenSquares(arr));
+console.log(evenSum(arr));
