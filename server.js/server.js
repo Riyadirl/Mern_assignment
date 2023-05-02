c44096b62bd483a3047c9579c336ad206a61e383
Mahbub Hasaan Riyad <riyad.info1@gmail.com>
2023-05-02 19:09:40 +0600

    .

---
+++ server.js/server.js
@@ -0,0 +1,50 @@
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+
+const publicDir = path.join(__dirname, "public");
+
+const server = http.createServer((req, res) => {
+  const filePath = path.join(
+    publicDir,
+    req.url === "/" ? "index.html" : req.url
+  );
+  const contentType = getContentType(filePath);
+
+  fs.readFile(filePath, (err, content) => {
+    if (err) {
+      if (err.code === "ENOENT") {
+        res.writeHead(404);
+        res.end("404 Not Found");
+      } else {
+        res.writeHead(500);
+        res.end("500 Internal Server Error");
+      }
+    } else {
+      res.writeHead(200, { "Content-Type": contentType });
+      res.end(content, "utf-8");
+    }
+  });
+});
+
+server.listen(3000, () => {
+  console.log("Hello, this is 3000 listening on port");
+});
+
+function getContentType(filePath) {
+  const extname = path.extname(filePath);
+  switch (extname) {
+    case ".html":
+      return "text/html";
+    case ".css":
+      return "text/css";
+    case ".js":
+      return "text/javascript";
+    case ".png":
+      return "image/png";
+    case ".jpg":
+      return "image/jpg";
+    default:
+      return "application/octet-stream";
+  }
+}
