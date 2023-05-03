f33d92e08d7a57c2e07e2873dd24b358f0722f41
Mahbub Hasaan Riyad <riyad.info1@gmail.com>
2023-05-03 20:27:00 +0600

    .

--- 005/script.js
+++ 005/script.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 function createServer() {
-    const server = http.createServer((req, res) => {
+    const server = http.createServer((req, res) =>{
         res.writeHead(200, { 'Content-Type': 'text/plain' });
         res.write('Hello World!');
         res.end();
