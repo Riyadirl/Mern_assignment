d053f433d34e89f419675f411dc4341c9b2cc6d4
Mahbub Hasaan Riyad <riyad.info1@gmail.com>
2023-05-08 23:14:21 +0600

    .

--- 001.js
+++ 001.js
@@ -1,13 +1,12 @@
-function multiplicationTable(num){
-    
-    n=10;
-    for(i=1;i<=n;i++){
-        var product= num * i;
-        console.log(num + " * " + i +" = " +product);
-    }
+function multiplicationTable(num) {
+  n = 10;
+  for (i = 1; i <= n; i++) {
+    var product = num * i;
+    console.log(num + " * " + i + " = " + product);
+  }
 }
 
 multiplicationTable(3);
 
 //different input
-multiplicationTable(9)
+multiplicationTable(67);
