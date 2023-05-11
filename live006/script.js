23120f1501a7b470685976f3966e7045d6267fa0
Mahbub Hasaan Riyad <riyad.info1@gmail.com>
2023-05-11 23:12:20 +0600

    live6

---
+++ live006/script.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const port = 8000;
+
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
+app.get("/users", (req, res) => {
+  const users = [
+    { name: "Mahbub", age: 23 },
+    { name: "Chintu", age: 2 },
+    { name: "Rocky", age: 2 },
+  ];
+
+  res.json(users);
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
