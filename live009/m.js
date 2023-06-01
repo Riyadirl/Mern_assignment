f21ebb6e0f3e9221ab90a5b1799b9da1f70a39b0
Mahbub Hasaan Riyad <riyad.info1@gmail.com>
2023-06-02 01:37:17 +0600

    live9

---
+++ live009/m.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 3,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  age: {
+    type: Number,
+    min: 18,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6,
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
