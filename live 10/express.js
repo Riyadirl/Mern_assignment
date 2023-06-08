6f6977bcbde66e4989d7a7482d8c92b659e807a5
Mahbub Hasaan Riyad <riyad.info1@gmail.com>
2023-06-08 23:28:11 +0600

    .

---
+++ live 10/express.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const jwt = require('jsonwebtoken');
+
+const app = express();
+
+app.get('/protected', authentication, (req, res) => {
+    res.json({ message: 'Successed' });
+});
+
+exports.authentication = (req, res, next) => {
+    try {
+        const decoded = jwt.verify(
+            req.headers.authorization,
+            process.env.JWT_SECRET
+        );
+
+        req.user = decoded;
+        next();
+    } catch (err) {
+        return res.status(401).json(err);
+    }
+};
