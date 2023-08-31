259e8d86ee2fa8a318925e24e2cace22a31b17d9
Mahbub Hasaan Riyad <riyad.info1@gmail.com>
2023-09-01 00:22:48 +0600

    20

--- module_20/src/models/UserModel.js
+++ module_20/src/models/UserModel.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
+const CartItem = require('./CartItemModel');
+const Order = require('./OrderModel');
 
 const DataSchema = new mongoose.Schema({
     firstName: { type: String, required: true },
@@ -9,13 +11,19 @@ const DataSchema = new mongoose.Schema({
 },
     { timestamps: true, versionKey: false });
 
+//encrypt pass
 DataSchema.pre("save", async function (next) {
     if (!this.isModified("password")) return next();
 
     const salt = await bcrypt.genSalt(10);
-
     this.password = await bcrypt.hash(this.password, salt);
+    next();
+});
 
+//remove after delete
+DataSchema.pre("remove", async function (next) {
+    await CartItem.deleteMany({ userId: this._id });
+    await Order.deleteMany({ userId: this._id });
     next();
 });
 
